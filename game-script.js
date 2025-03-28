488630b257d54c66928fa662a51e5655fdc76f09
PedroHenriqueJS <pedrohjs2013@gmail.com>
2025-03-28 16:38:01 -0300

    restart confirmation added

--- game-script.js
+++ game-script.js
@@ -3,7 +3,11 @@ const paperBt = document.querySelector('.paper')
 const scissorsBt = document.querySelector('.scissors')
 const resetBt = document.querySelector('.reset')
 const showScore = document.querySelector('.score')
+const autoPlayBt = document.querySelector('.auto-play')
+const showResetMessage = document.querySelector('.reset-confirmation')
+
 const score = JSON.parse(localStorage.getItem('score')) || { wins: 0, losses: 0, ties: 0 }
+
 const moves = {
     rock:'✊',
     paper: '🖐',
@@ -29,14 +33,13 @@ scissorsBt.addEventListener('click', () =>{
 })
 
 resetBt.addEventListener('click', () =>{
-    score.wins = 0
-    score.losses = 0
-    score.ties = 0
-    showScore.innerHTML = ''
-    localStorage.removeItem('score')
+    resetMessage()
 })
 
 
+autoPlayBt.addEventListener('click', () =>{
+    autoPlay()
+})
 
 function computerMoves(){
     const randomNum = Math.random()
@@ -76,7 +79,7 @@ function playerMoves(param){
             result = ' Tie.'
         }
     }
-
+    
     if(result === ' You win.'){
         score.wins += 1
     } else if (result === ' You lose.'){
@@ -84,12 +87,57 @@ function playerMoves(param){
     } else if (result === ' Tie.'){
         score.ties += 1
     }
-
+    
     localStorage.setItem('score', JSON.stringify(score))
-
+    
     showScore.innerHTML = `
     <p>${result}</p>
     <p>You <span style="font-size:2em">${param}</span> vs.<span style="font-size:2em">${computerMove}</span> Computer.</p>
     <p>Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}</p>`
 }
 
+let isAutoPlaying = false
+let intervalId
+function autoPlay() {
+    if(!isAutoPlaying){
+        intervalId = setInterval(() => {
+            const randomMove = Object.values(moves)[Math.floor(Math.random() * 3)];
+            computerMoves();
+            playerMoves(randomMove);
+        }, 1000);
+        isAutoPlaying = true
+        autoPlayBt.textContent = 'Stop Auto Play'
+    } else {
+        stopAutoPlay()
+    }
+}
+
+function stopAutoPlay(){
+    clearInterval(intervalId)
+    isAutoPlaying = false
+    autoPlayBt.textContent = 'Auto Play'
+}
+
+function resetMessage(){
+    showResetMessage.innerHTML = `
+    <p>Are you sure you want to reset the score?</p>
+    <button class="yes-button">Yes</button>
+    <button class="no-button">No</button>`
+
+    const yesBt = document.querySelector('.yes-button')
+    const noBt = document.querySelector('.no-button')
+    
+    yesBt.addEventListener('click', () =>{
+        score.wins = 0
+        score.losses = 0
+        score.ties = 0
+        showScore.innerHTML = ''
+        localStorage.removeItem('score')
+        stopAutoPlay()
+        showResetMessage.innerHTML = ''
+    })
+    
+    noBt.addEventListener('click', () => {
+        showResetMessage.innerHTML = ''
+    })
+}
